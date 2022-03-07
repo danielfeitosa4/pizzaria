b5e31ca91acc711364bbc21d32ebcdd58788d9f1
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2022-03-07 14:16:31 -0300

    criando animacao de carregamento

--- atendimento.js
+++
@@ -1,6 +0,0 @@
-var button = document.querySelector('button');
-
-button.onclick = function() {
-  var pedido = prompt('Qual é o seu pedido?');
-  alert('Olá ' + pedido + ', o seu pedido foi anotado!');
-}
